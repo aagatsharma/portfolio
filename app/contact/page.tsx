217f3beee5856d5fb8529640d53b9621189b6a89
aagatsharma <aagats72@gmail.com>
2024-02-16 12:29:21 +0545

    email js support added

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,9 +1,7 @@
+import ContactDetail from "@/components/contact/ContactDetail";
 import { BackgroundBeams } from "@/components/ui/background-beams";
-import { Button } from "@/components/ui/button";
-import { Input } from "@/components/ui/input";
-import { Label } from "@/components/ui/label";
-import { Textarea } from "@/components/ui/textarea";
-import React from "react";
+
+import React, { useRef } from "react";
 
 const contactPage = () => {
   return (
@@ -17,46 +15,7 @@ const contactPage = () => {
           {`If you want to know more about me or my work, or if you would just
           like to say hello, send me a message. I'd love to hear from you.`}
         </p>
-        {/* <input
-          type="text"
-          placeholder="aagats72@gmail.com"
-          className="rounded-lg border border-neutral-800 focus:ring-2 focus:ring-teal-500  w-full relative z-10 mt-4 p-5  bg-neutral-950 placeholder:text-neutral-700"
-        /> */}
-        <div className="grid w-full items-center gap-6">
-          <div className="z-10">
-            <Label className="z-10 text-lg text-primary/60">Name</Label>
-            <Input
-              type="text"
-              id="name"
-              placeholder="Enter your Name.."
-              className="z-10 w-full rounded-lg"
-            />
-          </div>
-          <div className="z-10">
-            <Label className="z-10 text-lg text-primary/60">Email</Label>
-            <Input
-              type="email"
-              id="email"
-              placeholder="Enter your email.."
-              className="z-10 w-full rounded-lg"
-            />
-          </div>
-          <div className="z-10">
-            <Label className="z-10 text-lg text-primary/60">Message</Label>
-            <Textarea
-              placeholder="Type your message here."
-              className="z-10 w-full"
-            />
-          </div>
-          <div className="z-10">
-            <button className="p-[3px] relative rounded-full">
-              <div className="absolute inset-0 bg-gradient-to-r from-red-600 to-purple-600 rounded-full" />
-              <div className="px-8 py-2  bg-primary-foreground rounded-full  relative group transition duration-200 text-white hover:bg-transparent">
-                Contact
-              </div>
-            </button>
-          </div>
-        </div>
+        <ContactDetail />
       </div>
       <BackgroundBeams />
     </div>
---
+++ components/contact/ContactDetail.tsx
@@ -0,0 +1,75 @@
+"use client";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { Textarea } from "@/components/ui/textarea";
+import emailjs from "@emailjs/browser";
+import { FormEvent, useRef, useState } from "react";
+const ContactDetail = () => {
+  const form = useRef<any>();
+  const [disable, setDisable] = useState(false);
+
+  const sendEmail = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm("service_vtgndoh", "template_e136xd7", form.current, {
+        publicKey: "g748fWlV2V3XKgPMc",
+      })
+      .then(
+        () => {
+          setDisable(true);
+        },
+        (error) => {
+          console.log("FAILED...", error.text);
+        }
+      );
+  };
+  return (
+    <form
+      className="grid w-full items-center gap-6"
+      ref={form}
+      onSubmit={sendEmail}
+    >
+      <div className="z-10">
+        <Label className="z-10 text-lg text-primary/60">Name</Label>
+        <Input
+          type="text"
+          name="user_name"
+          placeholder="Enter your Name.."
+          className="z-10 w-full rounded-lg"
+        />
+      </div>
+      <div className="z-10">
+        <Label className="z-10 text-lg text-primary/60">Email</Label>
+        <Input
+          type="email"
+          name="user_email"
+          placeholder="Enter your email.."
+          className="z-10 w-full rounded-lg"
+        />
+      </div>
+      <div className="z-10">
+        <Label className="z-10 text-lg text-primary/60">Message</Label>
+        <Textarea
+          name="message"
+          placeholder="Type your message here."
+          className="z-10 w-full"
+        />
+      </div>
+      <div className="z-10">
+        <button
+          className="p-[3px] relative rounded-full"
+          type="submit"
+          disabled={disable}
+        >
+          <div className="absolute inset-0 bg-gradient-to-r from-red-600 to-purple-600 rounded-full" />
+          <div className="px-8 py-2  bg-primary-foreground rounded-full  relative group transition duration-200 text-white hover:bg-transparent">
+            Contact
+          </div>
+        </button>
+      </div>
+    </form>
+  );
+};
+
+export default ContactDetail;
