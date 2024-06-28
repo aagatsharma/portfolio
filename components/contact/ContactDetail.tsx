2e071275471311b6e739d46426f62f50953acd94
aagatsharma <aagats72@gmail.com>
2024-06-28 11:53:53 +0545

    added validation to contact form

--- components/contact/ContactDetail.tsx
+++ components/contact/ContactDetail.tsx
@@ -1,83 +1,152 @@
 "use client";
 import { Input } from "@/components/ui/input";
-import { Label } from "@/components/ui/label";
 import { Textarea } from "@/components/ui/textarea";
 import emailjs from "@emailjs/browser";
 import Link from "next/link";
-import { FormEvent, useRef, useState } from "react";
+import { useRef, useState } from "react";
 import { Button } from "../ui/button";
+
+import { z } from "zod";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { toast } from "sonner";
+
 const ContactDetail = () => {
-  const form = useRef<any>();
+  const formRef = useRef<any>();
   const [disable, setDisable] = useState(false);
+  const formSchema = z.object({
+    username: z
+      .string()
+      .min(2, {
+        message: "Enter your name",
+      })
+      .max(50),
+    email: z.string().email({
+      message: "Enter Valid Email",
+    }),
+    message: z.string().min(1, {
+      message: "Enter a message",
+    }),
+  });
 
-  const sendEmail = (e: FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      username: "",
+      email: "",
+      message: "",
+    },
+  });
 
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    setDisable(true);
     emailjs
-      .sendForm("service_vtgndoh", "template_e136xd7", form.current, {
+      .sendForm("service_vtgndoh", "template_e136xd7", formRef.current, {
         publicKey: "g748fWlV2V3XKgPMc",
       })
       .then(
         () => {
-          setDisable(true);
+          toast.success("Email successfully sent");
+          form.reset();
         },
         (error) => {
-          console.log("FAILED...", error.text);
+          toast.error(error.text);
         }
-      );
-  };
+      )
+      .finally(() => {
+        setDisable(false);
+      });
+  }
   return (
-    <form
-      className="grid w-full items-center gap-6"
-      ref={form}
-      onSubmit={sendEmail}
-    >
-      <div className="z-10">
-        <Label className="z-10 text-lg text-primary/60">Name</Label>
-        <Input
-          type="text"
-          name="user_name"
-          placeholder="Enter your Name.."
-          className="z-10 w-full rounded-lg"
+    <Form {...form}>
+      <form
+        onSubmit={form.handleSubmit(onSubmit)}
+        className="grid w-full items-center gap-6"
+        ref={formRef}
+      >
+        <FormField
+          control={form.control}
+          name="username"
+          render={({ field }) => (
+            <FormItem className="z-10">
+              <FormLabel className="z-10 text-lg text-primary/60">
+                Name
+              </FormLabel>
+              <FormControl>
+                <Input placeholder="Enter your Name.." {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
         />
-      </div>
-      <div className="z-10">
-        <Label className="z-10 text-lg text-primary/60">Email</Label>
-        <Input
-          type="email"
-          name="user_email"
-          placeholder="Enter your email.."
-          className="z-10 w-full rounded-lg"
+        <FormField
+          control={form.control}
+          name="email"
+          render={({ field }) => (
+            <FormItem className="z-10">
+              <FormLabel className="z-10 text-lg text-primary/60">
+                Email
+              </FormLabel>
+              <FormControl>
+                <Input placeholder="Enter your Email.." {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
         />
-      </div>
-      <div className="z-10">
-        <Label className="z-10 text-lg text-primary/60">Message</Label>
-        <Textarea
+        <FormField
+          control={form.control}
           name="message"
-          placeholder="Type your message here."
-          className="z-10 w-full"
+          render={({ field }) => (
+            <FormItem className="z-10">
+              <FormLabel className="z-10 text-lg text-primary/60">
+                Message
+              </FormLabel>
+              <FormControl>
+                <Textarea
+                  placeholder="Send a Message"
+                  className="resize-none"
+                  {...field}
+                />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
         />
-      </div>
-      <div className="z-10">
-        <button
-          className="p-[3px] relative rounded-full"
-          type="submit"
-          disabled={disable}
-        >
-          <div className="absolute inset-0 bg-gradient-to-r from-red-600 to-purple-600 rounded-full" />
-          <div className="px-8 py-2  bg-primary-foreground rounded-full  relative group transition duration-200 text-white hover:bg-transparent">
-            Submit
-          </div>
-        </button>
-      </div>
-      <div className="z-10 mt-10">
-        <Link href="mailto:aagats72@gmail.com" target="_blank" className="z-10">
-          <Button variant={"shimmer"} type="button">
-            Send Mail Directly
-          </Button>
-        </Link>
-      </div>
-    </form>
+
+        <div className="z-10">
+          <button
+            className="p-[3px] relative rounded-full"
+            type="submit"
+            disabled={disable}
+          >
+            <div className="absolute inset-0 bg-gradient-to-r from-red-600 to-purple-600 rounded-full" />
+            <div className="px-8 py-2  bg-primary-foreground rounded-full  relative group transition duration-200 text-white hover:bg-transparent">
+              Submit
+            </div>
+          </button>
+        </div>
+        <div className="z-10 mt-10">
+          <Link
+            href="mailto:aagats72@gmail.com"
+            target="_blank"
+            className="z-10"
+          >
+            <Button variant={"shimmer"} type="button">
+              Send Mail Directly
+            </Button>
+          </Link>
+        </div>
+      </form>
+    </Form>
   );
 };
 
